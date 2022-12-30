afb9b31d00b96b5203d5a5532435f4e40ec7c7a3
Angell <70285261+DaniloCorvus@users.noreply.github.com>
2022-12-29 23:14:37 -0500

    working 11

--- src/app/demo/pages/form-elements/user/myowners/myowners.component.spec.ts
+++ src/app/demo/pages/form-elements/user/myowners/myowners.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MyownersComponent } from './myowners.component';
 
@@ -6,7 +6,7 @@ describe('MyownersComponent', () => {
   let component: MyownersComponent;
   let fixture: ComponentFixture<MyownersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyownersComponent ]
     })
